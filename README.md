c6135d945a026fc84a2beadc0cb1aae363f26e0e
vickyrules <VICKYRULES1705@GMAIL.COM>
2023-03-07 20:37:53 +0530

    Pokedex starter code

---
+++ README.md
@@ -0,0 +1,1 @@
+<div align="center"> <img src=".\app\src\main\res\drawable\ic_pokedex.png"></div>
--- app/src/main/java/com/material3/pokedex/MainActivity.kt
+++ app/src/main/java/com/material3/pokedex/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    PokedexApp()
                 }
             }
         }
@@ -31,7 +31,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PokedexApp(){
-    
+    PokedexTheme {
+
+    }
 }
 
 
